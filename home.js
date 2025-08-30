2b3d194fd60bc5294a0d5472b1afc4a7536b5cf4
Md Parvez <mdparvez8177@gmail.com>
2025-08-31 05:52:57 +0600

    add money function

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault();
+    const saveAcountNumber = 12345678901;
+    const savePin = 1234;
     const getBank = document.getElementById('select-bank').value;
     const getAcountNumber = document.getElementById('acount-number').value;
     const getAmount = parseInt(document.getElementById('add-amount').value);
@@ -7,6 +9,15 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
 
     const getBalance = parseInt(document.getElementById('balance').innerText);
     
+
+    if(saveAcountNumber !== getAcountNumber){
+        alert('Please provide correct Account Number');
+        return;
+    }
+    if(savePin !== getPin){
+        alert('Please provide correct PIN Number');
+        return
+    }
     const totalBalance = getBalance + getAmount;
     document.getElementById('balance').innerText = totalBalance;
     
