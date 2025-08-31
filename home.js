0061e2cc53939a7387825b0e60cfde56c036fb51
Md Parvez <mdparvez8177@gmail.com>
2025-09-01 04:44:36 +0600

    transaction

--- home.js
+++ home.js
@@ -24,6 +24,7 @@ function getInnerTextNumber(id) {
 // important variable
 const saveAccountNumber = 12345678901;
 const savePin = 1234;
+const transactionData = [];
 
 // Add Money
 
@@ -45,6 +46,14 @@ document.getElementById('add-money-btn').addEventListener('click', function (e) 
     }
     const totalBalance = getBalance + getAmount;
     document.getElementById('balance').innerText = totalBalance;
+
+    const data = {
+        name: "Add Money",
+        time: new Date().toLocaleTimeString()
+    };
+
+    transactionData.push(data);
+    console.log(transactionData);
 })
 
 
@@ -66,8 +75,15 @@ document.getElementById('withdraw-btn').addEventListener("click", function (e) {
         return
     }
     document.getElementById('balance').innerText = currentBalance;
-})
 
+    const data = {
+        name: "Cash Out",
+        time: new Date().toLocaleTimeString()
+    };
+
+    transactionData.push(data);
+    console.log(transactionData);
+})
 
 
 // toggle function
@@ -80,33 +96,53 @@ function toggle(id) {
     document.getElementById(id).style.display = "block";
 }
 
+
+function activated(id) {
+    const active = document.getElementsByClassName("activated");
+    for (const item of active) {
+        item.classList.remove("border-blue-500", "bg-[#0874f20d]");
+        item.classList.add("border-[#08080880]");
+    }
+    document.getElementById(id).classList.add("border-blue-500", "bg-[#0874f20d]")
+    document.getElementsById(id).classList.remove("border-[#08080880]");
+}
+
 // toggle
 
 document.getElementById('add-money-card-btn').addEventListener('click', function () {
     toggle('add-money-section');
+    activated('add-money-card-btn');
 })
 
 document.getElementById('cash-out-card-btn').addEventListener('click', function () {
     toggle('cash-out-section');
+    activated('cash-out-card-btn');
+
 
 })
 
 document.getElementById('transfer-card-btn').addEventListener('click', function () {
     toggle('transfer-money-section');
+    activated('transfer-card-btn');
 
 })
 
 document.getElementById('bonus-card-btn').addEventListener('click', function () {
     toggle('bonus-section');
+    activated('bonus-card-btn');
+
 
 })
 
 document.getElementById('pay-bill-card-btn').addEventListener('click', function () {
     toggle('pay-bill-section');
+    activated('pay-bill-card-btn');
+
 
 })
 
 document.getElementById('transaction-card-btn').addEventListener('click', function () {
     toggle('transaction-section');
+    activated('transaction-card-btn');
 
 })
