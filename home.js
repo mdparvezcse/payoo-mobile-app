e3e16e438da68df41bfa105fe845705d9190dc72
Md Parvez <mdparvez8177@gmail.com>
2025-08-31 16:43:34 +0600

    cashout section

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-document.getElementById('add-money-btn').addEventListener('click',function(e){
+document.getElementById('add-money-btn').addEventListener('click', function (e) {
     e.preventDefault();
     const saveAcountNumber = 12345678901;
     const savePin = 1234;
@@ -8,17 +8,54 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
     const getPin = parseInt(document.getElementById('pin').value);
 
     const getBalance = parseInt(document.getElementById('balance').innerText);
-    
 
-    if(saveAcountNumber !== getAcountNumber){
+
+    if (saveAcountNumber !== getAcountNumber) {
         alert('Please provide correct Account Number');
         return;
     }
-    if(savePin !== getPin){
+    if (savePin !== getPin) {
         alert('Please provide correct PIN Number');
         return
     }
     const totalBalance = getBalance + getAmount;
     document.getElementById('balance').innerText = totalBalance;
-    
+
+})
+
+// cash out function
+document.getElementById('withdraw-btn').addEventListener("click", function (e) {
+    e.preventDefault();
+    const savePin = 1234; 
+    const getAgentNumber = document.getElementById('agent-number').value;
+    const getWithdrawAmount = parseInt(document.getElementById('withdraw-amount').value);
+    const getWithdrawPin = parseInt(document.getElementById('withdraw-pin').value);
+
+    const getBalance = document.getElementById('balance').innerText;
+    const currentBalance = getBalance - getWithdrawAmount;
+    console.log(currentBalance)
+    console.log(length.getAgentNumber);
+    if(getAgentNumber.toString().length !== 11){
+        alert('Provide valid Agent Number');
+        return;
+    }
+    if(savePin !== getWithdrawPin){
+        alert('Please provide correct PIN');
+        return
+    }
+
+    document.getElementById('balance').innerText = currentBalance;
+
+})
+
+// toggle
+
+document.getElementById('add-money-card-btn').addEventListener('click',function(){
+    document.getElementById('add-money-section').style.display = "block"
+    document.getElementById('cash-out-section').style.display = "none";
+})
+
+document.getElementById('cash-out-card-btn').addEventListener('click',function(){
+    document.getElementById('cash-out-section').style.display = "block";
+    document.getElementById('add-money-section').style.display = "none"
 })
