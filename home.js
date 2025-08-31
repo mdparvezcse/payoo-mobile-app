013db40e6b8817aa1cd41599ff0a45cfe498f265
Md Parvez <mdparvez8177@gmail.com>
2025-08-31 06:00:29 +0600

    small function

--- home.js
+++ home.js
@@ -3,9 +3,9 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
     const saveAcountNumber = 12345678901;
     const savePin = 1234;
     const getBank = document.getElementById('select-bank').value;
-    const getAcountNumber = document.getElementById('acount-number').value;
+    const getAcountNumber = parseInt(document.getElementById('acount-number').value);
     const getAmount = parseInt(document.getElementById('add-amount').value);
-    const getPin = document.getElementById('pin').value;
+    const getPin = parseInt(document.getElementById('pin').value);
 
     const getBalance = parseInt(document.getElementById('balance').innerText);
     
